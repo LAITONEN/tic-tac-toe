f5506137b2278591c46ddcdc14862af932fc1c97
Eduard Rastoropov <eduard@rastoropov@gmail.com>
2021-08-14 22:13:05 +0300

    Added winner announcement

--- src/App.js
+++ src/App.js
@@ -10,32 +10,6 @@ function App() {
   const [whoseTurn, setWhoseTurn] = useState(1) // 1 - player1, 2 - player2
   const [winner, setWinner] = useState(null)
 
-  useEffect(() => {
-
-    if (values.filter(v => v !== 0).length < 5) return
-    const cellNumbersOfLastPlayerMarks = values
-      .map((v, i) => v === whoseTurn ? i + 1 : null)
-      .filter(v => v !== null)
-
-    const combinations = getHorizontalCombinations(boardSize).concat(getVerticalCombinations(boardSize).concat(getDiagonalCombinations(boardSize)))
-    let possibleCrossedCombinations = []
-    for (const mark of cellNumbersOfLastPlayerMarks) {
-      const newPossibleCrossedCombinations = combinations
-        .filter((combo, index) => {
-
-          return combo.join('').startsWith(mark)
-        })
-
-      possibleCrossedCombinations = possibleCrossedCombinations.concat(newPossibleCrossedCombinations)
-    }
-    const weHaveAWinner = possibleCrossedCombinations.some(combination => {
-
-      return combination.every(cellNumber => cellNumbersOfLastPlayerMarks.includes(cellNumber))
-    })
-
-    if (weHaveAWinner) setWinner(whoseTurn)
-  }, [values, setWinner])
-
   const createBoard = () => {
     const numberOfCells = boardSize * boardSize
     return Array(numberOfCells).fill(null, 0, numberOfCells).map((v, cellIndex) => {
@@ -57,21 +31,53 @@ function App() {
     })
   }
 
+  // this hook should not be executed when whoseTurn changes
+  useEffect(() => {
+    if (values.filter(v => v !== 0).length < 5) return
+    console.log('setting a winner')
+    const cellNumbersOfLastPlayerMarks = values
+      .map((v, i) => v === whoseTurn ? i + 1 : null)
+      .filter(v => v !== null)
+
+    const combinations = getHorizontalCombinations(boardSize).concat(getVerticalCombinations(boardSize).concat(getDiagonalCombinations(boardSize)))
+    let possibleCrossedCombinations = []
+    for (const mark of cellNumbersOfLastPlayerMarks) {
+      const newPossibleCrossedCombinations = combinations
+        .filter((combo, index) => {
+
+          return combo.join('').startsWith(mark)
+        })
+
+      possibleCrossedCombinations = possibleCrossedCombinations.concat(newPossibleCrossedCombinations)
+    }
+    const weHaveAWinner = possibleCrossedCombinations.some(combination => {
+
+      return combination.every(cellNumber => cellNumbersOfLastPlayerMarks.includes(cellNumber))
+    })
+
+    if (weHaveAWinner) setWinner(whoseTurn)
+  }, [whoseTurn, boardSize, values, setWinner])
+
+
   useEffect(() => {
     const numberOfCells = boardSize * boardSize
     setValues(Array(numberOfCells).fill(0, 0, numberOfCells))
-  }, [boardSize])
+  }, [boardSize, setValues])
 
+  // adding whoseTurn to dependencies causes infinite loop
   useEffect(() => {
     setWhoseTurn(whoseTurn === 1 ? 2 : 1)
   }, [values])
 
-
   return (
     <div className="Container">
       <div className="Content">
-        <div className="Turn">{`Turn: Player ${whoseTurn}`}</div>
-        {winner && <div className="Winner">{`Winner: Player ${winner}!`}</div>}
+
+        {
+          winner
+            ? <div className="Winner">{`Winner: Player ${winner}!`}</div>
+            : <div className="Turn">{`Turn: Player ${whoseTurn}`}</div>
+        }
         <div className="Board">
           {createBoard()}
         </div>
